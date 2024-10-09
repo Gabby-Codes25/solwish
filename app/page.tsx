ea04b5254e6cc9b75140c2a35b27dfd0643a9eed
Gabriel Obasi <gabrielobasi557@gmail.com>
2024-10-09 21:27:25 +0100

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Input from "./Input/page";
 import HeroSection from "./components/heroSection";
-import FirstHero from "./components/firstHero";
-import SecondHero from "./components/secondHero";
-import ThirdHero from "./components/thirdHero";
-import FourthSection from "./components/fourthSection";
-import FAQSection from "./components/FaqSection";
-import Footer from "./components/footer";
 
 
 export default function Home() {
