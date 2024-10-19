5a52b47c204f1d312ba3d01572e43ce946b15a75
Gabriel Obasi <gabrielobasi557@gmail.com>
2024-10-19 07:01:34 +0100

    new changes

--- app/components/WalletProvider.tsx
+++ app/components/WalletProvider.tsx
@@ -15,7 +15,7 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
-  const endpoint = clusterApiUrl('devnet'); // Correct endpoint setup
+  const endpoint = clusterApiUrl("mainnet-beta");
 
   return (
     <ConnectionProvider endpoint={endpoint}>
